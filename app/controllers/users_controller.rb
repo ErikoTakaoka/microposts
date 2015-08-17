d239dea6c4c2e5d157e674bd754a8c61a7c778ae
ErikoTakaoka <taka.hachi1123@gmail.com>
2015-08-17 07:10:26 +0000

    Followings-Followers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,7 +34,18 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
-
+  def followings
+    
+    @user  = User.find(params[:id])
+    @users = @user.following_users
+    render 'show_follow'
+  end
+  
+  def followers
+     @user  = User.find(params[:id])
+     @users = @user.follower_users
+     render 'show_follow'
+  end
   private
 
   def user_params
